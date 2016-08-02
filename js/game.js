4214d5ec21ac5a9725b2dad8fc970b8a1a9c7fec
Lucio Aquino <Lucio Aquino>
2016-08-02 19:32:55 -0300

    last instance of CityChasers v0.5

--- js/game.js
+++ js/game.js
@@ -7,14 +7,32 @@ document.body.appendChild(canvas);
 
 var rndNumber1 = 0;
 var rndNumber2 = 0;
-var RndNumber1 = function() {
-	rndNumber1 = Math.round((Math.random() * 5));
-}
-var RndNumber2 = function() {
-	rndNumber2 = Math.round((Math.random() * 5));
-}
+var RndNumber1 = function () {
+	rndNumber1 = Math.round((Math.random() * 5) + 1);
+};
+var RndNumber2 = function () {
+	rndNumber2 = Math.round((Math.random() * 5) + 1);
+};
 var startTimer1 = new Date().getTime() + (rndNumber1 * 1000);
 var startTimer2 = new Date().getTime() + (rndNumber2 * 1000);
+var coinTimer = new Date().getTime() + (0.5 * 1000);
+var coinSN = 1;
+
+//points to set triangle collider
+var triPointSelected = 1;
+var px1 = 255;
+var py1 = 132;
+
+var px2 = 210;
+var py2 = 188;
+
+var px3 = 317;
+var py3 = 201;
+
+var tspeed = 2;
+var tbool = false;
+
+
 
 // Background image
 var bgReady = false;
@@ -22,7 +40,7 @@ var bgImage = new Image();
 bgImage.onload = function () {
 	bgReady = true;
 };
-bgImage.src = "images/bg3.png";
+bgImage.src = "images/blocks.png";
 
 // player image
 var playerReady = false;
@@ -46,15 +64,62 @@ var enemyImage2 = new Image();
 enemyImage2.onload = function () {
 	enemyReady2 = true;
 };
-enemyImage2.src = "images/p1.png";
+enemyImage2.src = "images/t1.png";
 
-// edificio
-var buildingReady = false;
-var buildingImage = new Image();
-buildingImage.onload = function () {
-	buildingReady = true;
+// edificio1
+var b1Ready = false;
+var b1Image = new Image();
+b1Image.onload = function () {
+	b1Ready = true;
 };
-buildingImage.src = "images/b_180.png";
+b1Image.src = "images/b1.png";
+// edificio2
+var b2Ready = false;
+var b2Image = new Image();
+b2Image.onload = function () {
+	b2Ready = true;
+};
+b2Image.src = "images/b2.png";
+// edificio3
+var b3Ready = false;
+var b3Image = new Image();
+b3Image.onload = function () {
+	b3Ready = true;
+};
+b3Image.src = "images/b3.png";
+
+
+// plaza1
+var pl1Ready = false;
+var pl1Image = new Image();
+pl1Image.onload = function () {
+	pl1Ready = true;
+};
+pl1Image.src = "images/pl1.png";
+
+// plaza2
+var pl2Ready = false;
+var pl2Image = new Image();
+pl2Image.onload = function () {
+	pl2Ready = true;
+};
+pl2Image.src = "images/pl2.png";
+
+// arboles
+var tReady = false;
+var tImage = new Image();
+tImage.onload = function () {
+	tReady = true;
+};
+tImage.src = "images/t.png";
+
+
+var coinReady = false;
+var coinImage = new Image();
+coinImage.onload = function () {
+	coinReady = true;
+};
+coinImage.src = "images/c1.png"; //c2 c3 c4
 
 // Game objects
 var player = {
@@ -123,22 +188,20 @@ var reset = function () {
 	player.x = 222;
 	player.y = 305;
 
-	var rnd = Random(0,crossesFree.length);
-	enemy.x = crossesFree[rnd].x;
-	enemy.y = crossesFree[rnd].y;
-	rnd = Random(0,crossesFree.length);
-	enemy2.x = crossesFree[rnd].x;
-	enemy2.y = crossesFree[rnd].y;
+	resetM1();
+	resetM2();
 
 	score = 0;
 };
 var resetM1 = function() {
-	enemy.x = 32 + (Math.random() * (canvas.width - 64));
-	enemy.y = 32 + (Math.random() * (canvas.height - 64));
+	var rnd = Random(0,crossesFree.length);
+	enemy.x = crossesFree[rnd].x;
+	enemy.y = crossesFree[rnd].y;
 }
 var resetM2 = function() {
-	enemy2.x = 32 + (Math.random() * (canvas.width - 64));
-	enemy2.y = 32 + (Math.random() * (canvas.height - 64));
+	var rnd = Random(0,crossesFree.length);
+	enemy2.x = crossesFree[rnd].x;
+	enemy2.y = crossesFree[rnd].y;
 }
 
 var changeDirM1 = true;
@@ -166,7 +229,7 @@ var update = function (modifier) {
 		colliderBool = false;
 		p1=false;
 		p3=false;
-	}else if (40 in keysDown && p2) { // down arrow
+	} else if (40 in keysDown && p2) { // down arrow
 		//moves down
 		if(player.y >=450) { player.y = 450; }
 		player.y += player.speed * modifier;
@@ -201,6 +264,29 @@ var update = function (modifier) {
 		p4=false;
 	}else if (82 in keysDown) { reset(); }
 
+    //TRIANGLE MOVE POINTS
+    if (49 in keysDown) triPointSelected = 1;
+    if (50 in keysDown) triPointSelected = 2;
+    if (51 in keysDown) triPointSelected = 3;
+    
+    if (87 in keysDown) { //W
+        if(triPointSelected == 1) py1 -= tspeed;
+        if(triPointSelected == 2) py2 -= tspeed;
+        if(triPointSelected == 3) py3 -= tspeed;
+    } else if (65 in keysDown) { // A
+        if(triPointSelected == 1) px1 -= tspeed;
+        if(triPointSelected == 2) px2 -= tspeed;
+        if(triPointSelected == 3) px3 -= tspeed;
+    } else if (83 in keysDown) { // S
+        if(triPointSelected == 1) py1 += tspeed;
+        if(triPointSelected == 2) py2 += tspeed;
+        if(triPointSelected == 3) py3 += tspeed;
+    } else if (68 in keysDown) { // D
+        if(triPointSelected == 1) px1 += tspeed;
+        if(triPointSelected == 2) px2 += tspeed;
+        if(triPointSelected == 3) px3 += tspeed;
+    } 
+    
 	//CHANGE DIRECTION
 	//enemy1
 	if(!changeAxisM1) {
@@ -236,12 +322,12 @@ var update = function (modifier) {
 		if(changeDirM2) {
 			enemy2.x += (enemy2.speed / .5) * diagonalMove * modifier;
 			enemy2.y += (enemy2.speed / .5) * modifier;
-			enemyImage2.src = "images/p1.png";
+			enemyImage2.src = "images/t1.png";
 		}
 		else {
 			enemy2.x -= (enemy2.speed / .5) * diagonalMove * modifier;
 			enemy2.y -= (enemy2.speed / .5) * modifier;
-			enemyImage2.src = "images/p3.png";
+			enemyImage2.src = "images/t3.png";
 		}
 		if(enemy2.y >= 400) changeDirM2 = false;
 		else if(enemy2.y <= 0) changeDirM2 = true;
@@ -249,12 +335,12 @@ var update = function (modifier) {
 		if(changeDirM2) {
 			enemy2.y += (enemy2.speed / .5) * modifier;
 			enemy2.x -= (enemy2.speed / .5) * diagonalMove * modifier;
-			enemyImage2.src = "images/p2.png";
+			enemyImage2.src = "images/t2.png";
 		}
 		else {
 			enemy2.y -= (enemy2.speed / .5) * modifier;
 			enemy2.x += (enemy2.speed / .5) * diagonalMove * modifier;
-			enemyImage2.src = "images/p4.png";
+			enemyImage2.src = "images/t4.png";
 
 		}
 		if(enemy2.x >= 400) changeDirM2 = false;
@@ -284,10 +370,11 @@ var update = function (modifier) {
 	}
 
 	CrossChoices();
+    TBool();
+    
+}
 
-};
-
-function  isTouching(player,obj) {
+function isTouching(player,obj) {
 	if(	
 		player.x <= (obj.x + 16)
 		&& obj.x <= (player.x + 16)
@@ -297,6 +384,23 @@ function  isTouching(player,obj) {
 		else { return false; }
 }
 
+//----------------------------
+//   POINT TO TRIANGLE
+//----------------------------
+var TBool = function () {
+        if (AB()*BC()>0 && BC()*CA()>0) tbool = true;
+        else tbool = false;
+    }
+function AB() {
+    return (eval((player.y-py1)*(px2-px1)-(player.x-px1)*(py2-py1)))
+}
+function BC() {
+    return (eval((player.y-py2)*(px3-px2)-(player.x-px2)*(py3-py2)))
+}
+function CA() {
+return (eval((player.y-py3)*(px1-px3)-(player.x-px3)*(py1-py3)))
+}
+//----------------------------
 
 
 var colliderBool = false;
@@ -305,6 +409,12 @@ var render = function () {
 	if (bgReady) {
 		ctx.drawImage(bgImage, 0, 0);
 	}
+
+	//dibuja la moneda
+	ctx.drawImage(coinImage,245,148); 
+	ctx.drawImage(coinImage,305,185); 
+	ctx.drawImage(coinImage,363,230); 
+
 	if (playerReady) {
 		ctx.drawImage(playerImage, player.x, player.y);
 	}
@@ -314,14 +424,46 @@ var render = function () {
 	if (enemyReady2) {
 		ctx.drawImage(enemyImage2, enemy2.x, enemy2.y);
 	}
-	if (buildingReady) {
-		//ctx.drawImage(buildingImage,46,16);
+	if (b1Ready) { //RED BUILDING
+		ctx.drawImage(b1Image,83,55);
+		ctx.drawImage(b1Image,-37,-30);
+		ctx.drawImage(b1Image,203,-30);
+		ctx.drawImage(b1Image,83,-115);
+	}
+	if (b2Ready) { //BLACK BUILDING
+		ctx.drawImage(b2Image,323,55);
+		ctx.drawImage(b2Image,443,-30);
+		ctx.drawImage(b2Image,323,-115);
+		ctx.drawImage(b2Image,443,140);
+	}
+	if (b3Ready) { //ORANGE BUILDINGS
+		ctx.drawImage(b3Image,323,395);
+		ctx.drawImage(b3Image,83,395);
+		ctx.drawImage(b3Image,-37,310);
+		ctx.drawImage(b3Image,443,310);
+	}
+	if (pl1Ready) { //
+		ctx.drawImage(pl1Image,205,210);
+	}
+	if (pl2Ready) { //
+		ctx.drawImage(pl2Image,-17,210);
+	}
+	if (tReady) { //
+		ctx.drawImage(tImage,97,285);
+		ctx.drawImage(tImage,337,285);
+		ctx.drawImage(tImage,217,370);
 	}
 
+	//render of triangle points
+    ctx.fillRect(px1,py1,5,5);
+    ctx.fillRect(px2,py2,5,5);
+    ctx.fillRect(px3,py3,5,5);
+
 	var endTimer = new Date().getTime();
-	var timer1 = Math.round((startTimer1 - endTimer)/1000);
-	var timer2 = Math.round((startTimer2 - endTimer)/1000);
-	if(timer1 <= 0) {
+	//var timer1 = Math.round((startTimer1 - endTimer)/1000);
+	//var timer2 = Math.round((startTimer2 - endTimer)/1000);
+	var timer3 = Math.round((coinTimer - endTimer)/1000);
+	/*if(timer1 <= 0) {
 		RndNumber1();
 		startTimer1 = new Date().getTime() + (rndNumber1 * 1000);
 		if(changeAxisM1) {
@@ -338,18 +480,34 @@ var render = function () {
 		} else {
 			changeAxisM2 = true;
 		}
+	}*/
+
+	if(timer3 <= 0) {
+		coinTimer = new Date().getTime() + (0.6 * 1000);
+		coinSN++;
+		if(coinSN > 4) coinSN = 1;
+		if(coinSN === 1) { coinImage.src = "images/c1.png"; }
+		else if(coinSN === 2) { coinImage.src = "images/c2.png"; }
+		else if(coinSN === 3) { coinImage.src = "images/c3.png"; }
+		else if(coinSN === 4) { coinImage.src = "images/c4.png"; }
 	}
 
 	// Score
-	ctx.fillStyle = "white";
+	ctx.fillStyle = "green";
 	ctx.font = "16px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
 	ctx.fillText("Score: " + score, 32, 32);
-	ctx.fillText("p1: " + p1, 32,64);
+	/*ctx.fillText("p1: " + p1, 32,64);
 	ctx.fillText("p2: " + p2, 32,96);
 	ctx.fillText("p3: " + p3, 32,128);
-	ctx.fillText("p4: " + p4, 32,160);
+	ctx.fillText("p4: " + p4, 32,160);*/
+    
+    //MUESTRO LA POSICION DE CADA PUNTA DEL TRIANGULO
+    ctx.fillText("Point1 _ X: " + px1 + " Y: " + py1, 32,64);
+	ctx.fillText("Point2 _ X: " + px2 + " Y: " + py2, 32,96);
+	ctx.fillText("Point3 _ X: " + px3 + " Y: " + py3, 32,128);
+	ctx.fillText("triangle collision?: " + tbool,32,160);
 };
 
 // base del juego. Update y Render
@@ -365,105 +523,7 @@ var main = function () {
 	requestAnimationFrame(main);
 };
 
-
-
-
-
-
-
-
 var CrossChoices = function() {
-
-
-	/*for (i=0;i<crossesP1.length ;i++ )
-		{
-			if (isTouching(player,crossesP1[i]))
-			{
-				colliderBool = true;
-				p1=true;
-				p2=false;
-				p3=false;
-				p4=false;
-			}
-		}
-	for (i=0;i<crossesP1P4.length ;i++ )
-		{
-			if (isTouching(player,crossesP1P4[i]))
-			{
-				colliderBool = true;
-				p1=true;
-				p2=false;
-				p3=false;
-				p4=true;
-			}
-		}
-	for (i=0;i<crossesP4.length ;i++ )
-		{
-			if (isTouching(player,crossesP4[i]))
-			{
-				colliderBool = true;
-				p1=false;
-				p2=false;
-				p3=false;
-				p4=true;
-			}
-		}
-	for (i=0;i<crossesP1P2.length ;i++ )
-		{
-			if (isTouching(player,crossesP1P2[i]))
-			{
-				colliderBool = true;
-				p1=true;
-				p2=true;
-				p3=false;
-				p4=false;
-			}
-		}
-	for (i=0;i<crossesP4P3.length ;i++ )
-		{
-			if (isTouching(player,crossesP4P3[i]))
-			{
-				colliderBool = true;
-				p1=false;
-				p2=false;
-				p3=true;
-				p4=true;
-			}
-		}
-	for (i=0;i<crossesP2.length ;i++ )
-		{
-			if (isTouching(player,crossesP2[i]))
-			{
-				colliderBool = true;
-				p1=false;
-				p2=true;
-				p3=false;
-				p4=false
-			}
-		}
-	for (i=0;i<crossesP2P3.length ;i++ )
-		{
-			if (isTouching(player,crossesP2P3[i]))
-			{
-				colliderBool = true;
-				p1=false;
-				p2=true;
-				p3=true;
-				p4=false;
-			}
-		}
-	for (i=0;i<crossesP3.length ;i++ )
-		{
-			if (isTouching(player,crossesP3[i]))
-			{
-				colliderBool = true;
-				p1=false;
-				p2=false;
-				p3=true;
-				p4=false;
-			}
-		}
-	*/
 	for (i=0;i<crossesFree.length ;i++ )
 		{
 			if (isTouching(player,crossesFree[i]))
@@ -475,19 +535,7 @@ var CrossChoices = function() {
 				p4=true;
 			}
 		}
-
 };
-
-
-
-
-
-
-
-
-
-
-
 
 
 
